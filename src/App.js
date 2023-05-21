fe5c81f251bbfa733fe39dedd20ec32958a1b214
Rudiuk Hanna <melomanna@gmail.com>
2023-05-21 11:04:49 +0300

    API url was added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import Footer from "./components/Footer";
 
+const BASE_URL = "https://api.themoviedb.org/3/";
+const API_KEY = "api_key=c28f2b3a5f12a97c881515e0374f688c";
+const API_URL = `${BASE_URL}movie/popular?${API_KEY}`;
+const API_SEARCH = `${BASE_URL}search/movie?${API_KEY}`;
+const API_YEAR = `${BASE_URL}discover/movie?${API_KEY}`;
 
 function App() {
   return (
-    <div>
+    <>
       
-    </div>
+      <Footer/>
+    </>
   );
 }
 
