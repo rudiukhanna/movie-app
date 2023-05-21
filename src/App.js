0a8ffd005d8ff6b3f4a93a04b8d6d41e8c01569b
Rudiuk Hanna <melomanna@gmail.com>
2023-05-21 12:31:00 +0300

    styles SCSS was added to the app

--- src/App.js
+++ src/App.js
@@ -14,6 +14,85 @@ const API_URL = `${BASE_URL}movie/popular?${API_KEY}`;
 const API_SEARCH = `${BASE_URL}search/movie?${API_KEY}`;
 const API_YEAR = `${BASE_URL}discover/movie?${API_KEY}`;
 
+const genres =  [
+  {
+    "id": 28,
+    "name": "Action"
+  },
+  {
+    "id": 12,
+    "name": "Adventure"
+  },
+  {
+    "id": 16,
+    "name": "Animation"
+  },
+  {
+    "id": 35,
+    "name": "Comedy"
+  },
+  {
+    "id": 80,
+    "name": "Crime"
+  },
+  {
+    "id": 99,
+    "name": "Documentary"
+  },
+  {
+    "id": 18,
+    "name": "Drama"
+  },
+  {
+    "id": 10751,
+    "name": "Family"
+  },
+  {
+    "id": 14,
+    "name": "Fantasy"
+  },
+  {
+    "id": 36,
+    "name": "History"
+  },
+  {
+    "id": 27,
+    "name": "Horror"
+  },
+  {
+    "id": 10402,
+    "name": "Music"
+  },
+  {
+    "id": 9648,
+    "name": "Mystery"
+  },
+  {
+    "id": 10749,
+    "name": "Romance"
+  },
+  {
+    "id": 878,
+    "name": "Science Fiction"
+  },
+  {
+    "id": 10770,
+    "name": "TV Movie"
+  },
+  {
+    "id": 53,
+    "name": "Thriller"
+  },
+  {
+    "id": 10752,
+    "name": "War"
+  },
+  {
+    "id": 37,
+    "name": "Western"
+  }
+  ]
+
 function App() {
   const [movies, setMovies]=useState([]);
   const [selectedGenres, setSelectedGenres] = useState([]);
@@ -62,6 +141,13 @@ function App() {
   const yearChangeHandler = (e) => {
     setYear(e.target.value);
   };
+
+  useEffect(() => {
+    const storedFavorites = JSON.parse(localStorage.getItem("favorites")); 
+    if (storedFavorites) {
+      setFavorites(storedFavorites);
+    }
+  }, []);
 
   const addToFavorites = (movie) => {
     console.log(movie.id)
--- src/components/MovieBox.js
+++ src/components/MovieBox.js
@@ -3,12 +3,18 @@ import { useState } from "react";
 
 export const API_IMG = "https://image.tmdb.org/t/p/w500/";
 
-function MovieBox({title, poster_path, vote_average, release_date, overview}) {
+function MovieBox({title, poster_path, vote_average, release_date, overview, addToFavorites, id }) {
 
     const [show, setShow] = useState(false);
+    const [isFavorite, setIsFavorite] = useState(false);
 
     const handleShow=()=>setShow(true);
     const handleClose=()=>setShow(false);
+
+    const handleAddToFavorites = () => {
+        setIsFavorite(true);   
+        addToFavorites({ title, poster_path, vote_average, release_date, overview, id });
+     };
 
     return (
         <div className="card text-center mb-3">
@@ -29,6 +35,26 @@ function MovieBox({title, poster_path, vote_average, release_date, overview}) {
                       <h6>Overview</h6>
                       <p>{overview}</p>
                       <br></br>
+                      { isFavorite ? (
+                        <p>This movie is already in your favorites</p>
+                      )  : (
+                        <button className="btn btn-outline-danger" onClick={handleAddToFavorites}>
+                                Add to Favorites
+                                <svg
+                                      width='1em'
+                                      height='1em'
+                                      viewBox='0 0 16 16'
+                                      className='bi bi-heart-fill'
+                                      fill='red'
+                                      xmlns='http://www.w3.org/2000/svg'
+                                    >
+                                      <path
+                                        fill-rule='evenodd'
+                                        d='M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z'
+                                      />
+                                 </svg>
+                        </button>
+                     )}
                       </Modal.Body>
                       <Modal.Footer>
                           <Button variant="secondary" onClick={handleClose}>Close</Button>
@@ -38,7 +64,7 @@ function MovieBox({title, poster_path, vote_average, release_date, overview}) {
             </div>
         
         </div>
-    )
+    );
 }
 
 export default MovieBox;
