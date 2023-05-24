9a4512413fdb29b6a83c47ddf04aec89444d5e60
Rudiuk Hanna <melomanna@gmail.com>
2023-05-24 21:08:11 +0300

    src/index.js was changed

--- src/index.js
+++ src/index.js
@@ -2,32 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {
-  BrowserRouter,
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import reportWebVitals from './reportWebVitals';
 import AboutPage from './components/AboutPage';
 
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-  },
-  {
-    path: "about",
-    element: <AboutPage />,
-  },
-]);
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
- 
-    <RouterProvider router={router} />
-
+ReactDOM.render(
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <App />
+  </BrowserRouter>,
+  document.getElementById('root')
 );
 
-
 reportWebVitals();
+
